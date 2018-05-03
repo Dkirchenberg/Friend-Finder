e0a103071ec814b7c5360c2e56185850c7ca50c8
Dkirchenberg <dekirchenberg@gmail.com>
2018-05-02 21:55:54 -0400

    first round

---
+++ app/data/friends.js
@@ -0,0 +1,155 @@
+var friendsArray = [{
+    "name": "Nancy",
+    "photo": "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        5,
+        1,
+        1,
+        4,
+        3,
+        1,
+        2,
+        1,
+        4,
+        3
+    ]
+}, {
+    "name": "Kelly",
+    "photo": "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    "scores": [
+        1,
+        1,
+        4,
+        1,
+        5,
+        1,
+        2,
+        1,
+        4,
+        1
+    ]
+},{
+    "name": "Kurt",
+    "photo": "https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        1
+    ]
+},{
+    "name": "Michael",
+    "photo": "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4
+    ]
+},{
+    "name": "Mark",
+    "photo": "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+},{
+    "name": "Carol",
+    "photo": "https://images.pexels.com/photos/209037/pexels-photo-209037.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+},{
+    "name": "Karen",
+    "photo": "https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        5,
+        4,
+        5,
+        4,
+        5,
+        4,
+        5,
+        4,
+        5,
+        4
+    ]
+},{
+    "name": "David",
+    "photo": "http://www.bangaloremirror.com/photo/27168334.cms",
+    "scores": [
+        3,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        2
+    ]
+},{
+    "name": "John",
+    "photo": "https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},{
+    "name": "Brent",
+    "photo": "https://images.pexels.com/photos/326875/pexels-photo-326875.jpeg?auto=compress&cs=tinysrgb&h=350",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+}
+
+];
+
+module.exports = friendsArray;
